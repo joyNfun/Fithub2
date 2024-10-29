e7aa72d44cd93a28f7a2d8211a78859a0f63c52e
joyNfun <152051896+joyNfun@users.noreply.github.com>
2024-10-29 02:06:59 -0400

    Add files via upload

--- js/scripts.js
+++ js/scripts.js
@@ -5,6 +5,6 @@ const toggleButton = document.querySelector('.menu-toggle');
 const navMenu = document.querySelector('nav ul');
 
 toggleButton.addEventListener('click', () => {
-    console.log('Toggle button clicked'); // Debugging log
+    console.log('Toggle button clicked'); 
     navMenu.classList.toggle('show-menu');
 });
